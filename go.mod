a31f2b54133c96c138a01385c7d389c7199bb2fb
Ivan Mishin <imishin@samokat.ru>
2024-09-02 12:17:16 +0300

    wip

--- go.mod
+++ go.mod
@@ -8,3 +8,11 @@ require (
 )
 
 require github.com/go-chi/chi/v5 v5.1.0
+
+require (
+	github.com/mfridman/interpolate v0.0.2 // indirect
+	github.com/pressly/goose/v3 v3.21.1 // indirect
+	github.com/sethvargo/go-retry v0.2.4 // indirect
+	go.uber.org/multierr v1.11.0 // indirect
+	golang.org/x/sync v0.7.0 // indirect
+)
--- internal/storage/storage.go
+++ internal/storage/storage.go
@@ -2,26 +2,45 @@ package storage
 
 import (
 	"database/sql"
+	"embed"
 	"log/slog"
 	"os"
 
 	_ "github.com/lib/pq"
+	goose "github.com/pressly/goose/v3"
 )
 
 type DB struct {
 	driver *sql.DB
 }
 
+//go:embed migrations/*.sql
+var embedMigrations embed.FS
+
+func upMigrations(driver *sql.DB) {
+	goose.SetBaseFS(embedMigrations)
+
+	if err := goose.SetDialect("postgres"); err != nil {
+		slog.Error("Error when set dialect", "err", err)
+		os.Exit(1)
+	}
+
+	if err := goose.Up(driver, "migrations"); err != nil {
+		slog.Error("Error when up migrations", "err", err)
+		os.Exit(1)
+	}
+}
+
 func MakeDB(dsn string) DB {
-	slog.Info("dsn for db", "dsn", dsn)
 	driver, err := sql.Open("postgres", dsn)
 	if err != nil {
 		slog.Error("Error when open database")
 		os.Exit(1)
 	}
 
+	upMigrations(driver)
+
 	return DB{
 		driver: driver,
 	}
-
 }
