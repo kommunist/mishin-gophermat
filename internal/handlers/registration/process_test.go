45af7d58e047e1384de83de3cbcba9323bc950bd
kommunist <communist.mishin@yandex.ru>
2024-09-05 16:06:43 +0300

    [registration_test] more tests (#3)
    Co-authored-by: Ivan Mishin <imishin@samokat.ru>

--- internal/handlers/registration/process_test.go
+++ internal/handlers/registration/process_test.go
@@ -4,7 +4,9 @@ import (
 	"bytes"
 	"context"
 	"encoding/json"
+	"errors"
 	"mishin-gophermat/internal/config"
+	"mishin-gophermat/internal/errors/exist"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -14,7 +16,7 @@ import (
 )
 
 func TestProcess(t *testing.T) {
-	t.Run("Start_POST_to_create_user_in_db", func(t *testing.T) {
+	t.Run("create_user_in_db_happy_path_200", func(t *testing.T) {
 		// создали конфиг и стор
 		c := config.MakeConfig()
 		stor := NewMockAbstrStorage(gomock.NewController(t))
@@ -42,7 +44,7 @@ func TestProcess(t *testing.T) {
 		defer res.Body.Close()
 
 		// Проверяем статус ответа
-		assert.Equal(t, http.StatusOK, res.StatusCode)
+		assert.Equal(t, http.StatusOK, res.StatusCode) // 200
 		// Проверяем куку
 		cookie := res.Cookies()[0]
 		assert.Equal(t, "jwt", cookie.Name)
@@ -53,4 +55,76 @@ func TestProcess(t *testing.T) {
 		assert.Equal(t, "BEARER "+cookie.Value, header)
 
 	})
+
+	t.Run("create_user_in_db_incorrect_format_400", func(t *testing.T) {
+		// создали конфиг и стор
+		c := config.MakeConfig()
+		stor := NewMockAbstrStorage(gomock.NewController(t))
+
+		// заинитили хендлер
+		h := InitHandler(c, stor)
+
+		// подготовили данные для запроса и сам запрос
+		ctx := context.Background()
+		request :=
+			httptest.NewRequest(
+				http.MethodPost,
+				"/api/user/register",
+				bytes.NewReader([]byte("vupsenpupsen")), // а запрос то кривой
+			).WithContext(ctx)
+
+		// т.е. ожидаем, что запроса в базу не будет
+		stor.EXPECT().CreateUser(ctx, "Login", "Password").Times(0)
+
+		// Делаем запрос
+		w := httptest.NewRecorder()
+		h.Process(w, request)
+		res := w.Result()
+		defer res.Body.Close()
+
+		// Проверяем статус ответа
+		assert.Equal(t, http.StatusBadRequest, res.StatusCode) // 400
+	})
+
+	t.Run("create_user_in_db_already_exist_409", func(t *testing.T) {
+		// создали конфиг и стор
+		c := config.MakeConfig()
+		stor := NewMockAbstrStorage(gomock.NewController(t))
+
+		// заинитили хендлер
+		h := InitHandler(c, stor)
+
+		// подготовили данные для запроса и сам запрос
+		inputJSON, _ := json.Marshal(requestItem{Login: "Login", Password: "Password"})
+		ctx := context.Background()
+		request :=
+			httptest.NewRequest(
+				http.MethodPost,
+				"/api/user/register",
+				bytes.NewReader(inputJSON),
+			).WithContext(ctx)
+
+		// ожидаем, что в базу будет такой поход
+		stor.EXPECT().CreateUser(
+			ctx, "Login", "Password",
+		).Return(exist.NewExistError(errors.New("qq")))
+
+		// Делаем запрос
+		w := httptest.NewRecorder()
+		h.Process(w, request)
+		res := w.Result()
+		defer res.Body.Close()
+
+		// Проверяем статус ответа
+		assert.Equal(t, http.StatusConflict, res.StatusCode) // 409
+		// Проверяем отсутствие куки
+		cookies := res.Cookies()
+		assert.Equal(t, 0, len(cookies))
+
+		// Проверяем, что хедера нет
+		header := res.Header.Get("Authorization")
+		assert.Equal(t, "", header)
+
+	})
+
 }
--- internal/storage/access.go
+++ internal/storage/access.go
@@ -2,7 +2,11 @@ package storage
 
 import (
 	"context"
+	"errors"
 	"log/slog"
+	"mishin-gophermat/internal/errors/exist"
+
+	"github.com/lib/pq"
 )
 
 func (db *DB) CreateUser(ctx context.Context, login string, password string) error {
@@ -10,6 +14,13 @@ func (db *DB) CreateUser(ctx context.Context, login string, password string) err
 		"INSERT INTO users (login, password) VALUES ($1, $2)",
 		login, password,
 	)
+
+	var pqErr *pq.Error
+
+	if errors.As(err, &pqErr) && pqErr.Code.Name() == "unique_violation" {
+		slog.Info("User not uniq login", "login", login)
+		return exist.NewExistError(err)
+	}
 
 	if err != nil { // необходимо добавит обработку ситуации, когда пользователь уже есть
 		slog.Error("Error when create user", "err", err)
