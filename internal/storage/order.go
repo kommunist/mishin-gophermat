d5593b392dcdd56fadbeda6adbc8c5fc79c76e27
Ivan Mishin <imishin@samokat.ru>
2024-09-17 20:37:00 +0300

    [accrual] fix

--- internal/storage/order.go
+++ internal/storage/order.go
@@ -95,7 +95,7 @@ func (db *DB) CreateOrder(ctx context.Context, number string, userLogin string) 
 
 func (db *DB) UpdateOrderStatusAndValue(ctx context.Context, number string, status string, value float64) error {
 	_, err := db.driver.ExecContext(ctx,
-		"UPDATE orders SET (status, value) VALUES ($1, $2) where number = $3",
+		"UPDATE orders SET status = $1, value = $2 WHERE number = $3",
 		status, value, number,
 	)
 
